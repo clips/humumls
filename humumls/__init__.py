97048af944b4f5be403f3e15343f18acb5ca8c4d
stephantul <stephan.tulkens@uantwerpen.be>
2018-11-13 13:41:11 +0100

    Update humumls/__init__.py

--- humumls/__init__.py
+++ humumls/__init__.py
@@ -1,5 +1,6 @@
 """Umls within mongoDB."""
 from .table import Concept, String, Term
 from .tablecreator import createdb
+from .db import Db
 
-__all__ = ["Concept", "String", "Term", "createdb"]
+__all__ = ["Concept", "String", "Term", "createdb", "Db"]
