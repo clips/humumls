37e29b8c55fdff6eb78c0a4c14bc73b40aa87d70
stephantul <stephan.tulkens@uantwerpen.be>
2018-11-13 13:24:42 +0100

    refactor

--- humumls/db.py
+++ humumls/db.py
@@ -6,7 +6,7 @@ from humumls.connection import Connection
 from humumls import String, Term, Concept
 
 
-class Aggregator(object):
+class Db(object):
     """Example class of Aggregate queries using different UMLS tables."""
 
     def __init__(self, name="umls", hostname="localhost", port=27017):
@@ -122,3 +122,19 @@ class Aggregator(object):
             output[c["_id"]] = list(output[c["_id"]])
 
         return output
+
+    def get_child_words(self, string):
+        """Get all words which are children of a word."""
+        cuis = self.string.cuis(string)
+        children = [x['rel']['child'] for x in self.concept.bunch(cuis)]
+        children = list(chain.from_iterable(children))
+
+        children_strings = [x['string'] for x in self.concept.bunch(children)]
+        return list(chain.from_iterable(children_strings))
+
+    def get_all_children(self, cui):
+        """Recursively get all children of a cui."""
+        cuis = [cui]
+        for x in self.concept.retrieve_one(cui)['rel']['child']:
+            cuis.extend(self.get_all_children(x))
+        return cuis
